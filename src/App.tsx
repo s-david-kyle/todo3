876fa2c03ef43475df932dc8242a23965a8efbbb
S. David Kyle <sdavidkyle@gmail.com>
2024-07-28 14:46:15 -0700

    feat: Refactor TaskList and Counter components to use reducers
    This commit refactors the TaskList and Counter components to use reducers instead of useState for managing state. The Counter component now uses the counterReducer to handle increment and reset actions, while the TaskList component uses the tasksReducer to handle add and delete actions. This enhancement improves the code structure and maintainability by separating the state logic into dedicated reducers.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
-  );
+  return <TaskList></TaskList>;
 }
 
 export default App;
