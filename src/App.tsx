8b67aa5d10e06adf1932fab46c61de6bb682df0c
S. David Kyle <sdavidkyle@gmail.com>
2024-07-28 18:46:09 -0700

    refactor: Update file imports for TaskList and NavBar components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import TaskProvider from "./state-management/tasks/TaskProvider";
+import { TaskProvider } from "./state-management/tasks";
 
 function App() {
   return (
